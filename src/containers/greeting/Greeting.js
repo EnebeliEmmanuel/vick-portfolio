3f77aaf9f264f7327540e4004381b22e53eda21e
Elevatedpeace <Peace08182443958>
2022-03-29 21:28:35 +0100

    cool

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -51,8 +51,8 @@ export default function Greeting() {
           <div className="greeting-image-div">
             <img
               alt="man sitting on table"
-              src={require("https://user-images.githubusercontent.com/58889001/160700775-f9d947b9-d220-4021-99df-4f822d8b8882.png")}
-            ></img>
+              src="https://user-images.githubusercontent.com/58889001/160700775-f9d947b9-d220-4021-99df-4f822d8b8882.png"
+            />
           </div>
         </div>
       </div>
