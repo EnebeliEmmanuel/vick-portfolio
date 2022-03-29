196329e6415a4d59febb250ead2d81d765b5900a
Elevatedpeace <Peace08182443958>
2022-03-29 20:15:43 +0100

    cool

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,6 +8,7 @@ import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
+import Blockchain  from "./Blockchain/blockChainEvents";
 import Footer from "../components/footer/Footer";
 import Talks from "./talks/Talks";
 import Podcast from "./podcast/Podcast";
@@ -55,6 +56,7 @@ export default class Main extends Component {
           <StartupProject />
           <Achievement />
           <Blogs />
+          <Blockchain />
           <Talks />
           <Twitter />
           <Podcast />
